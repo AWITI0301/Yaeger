56d9deab31fa841f0bda60756cab42688d6505bf
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-07 14:12:30 +0300

    import navbar components and modify the paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import NavBar from './components/NavBar';
 import { UserProvider } from './context/User';
-
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import GamesContainer from './components/Game/GamesContainer';
+import GameCard from './components/Game/GameCard';
+import CommentForm from './components/Comment/CommentForm';
+import GameForm from './components/Game/GameForm';
+import CommentContainer from './components/Comment/CommentContainer';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
