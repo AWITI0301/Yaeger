b3409e42db3b778e23c2f8e51b13305128178bc3
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-08 13:46:54 +0300

    update navbar content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,47 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import {UserContext} from '../context/user';
-import "./css/style.css";
+import React,{useContext}from 'react'
+import {NavLink} from 'react-router-dom'
+import { UserContext } from '../context/User'
+import "./css/style.css"
 
-function NavBar () {
+function NavBar(){
     const {user} = useContext(UserContext)
 
- 
     return(
         <div className='nav-bar'>
-        <NavLink to ="/">
-            Home
-        </NavLink>
+            <NavLink to ="/">
+                Home
+            </NavLink>
 
-        <NavLink to ="/games">
-            Games
-        </NavLink>
+            <NavLink to ="/games">
+                Games
+            </NavLink>
 
-       
+           
 
-        <NavLink to ="/comments">
-            Comments
-        </NavLink>
+            <NavLink to ="/comments">
+                Comments
+            </NavLink>
 
-        
-       {user ? <NavLink
-       activeStyle={{
-           fontWeight:"bolder",
-           color: "red"
-       }}
-      to="/logout"
-       >Logout</NavLink> : (<NavLink
-       activeStyle={{
-           fontWeight:"bolder",
-           color: "red"
-       }}
-      to="/signup"
-       >SignUp</NavLink> )}
+            
+           {user ? <NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+          to="/logout"
+           >Logout</NavLink> : (<NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+          to="/signup"
+           >SignUp</NavLink> )}
 
-       <NavLink to ="/game/new">
-            New Game
-        </NavLink>
+           <NavLink to ="/game/new">
+                New Game
+            </NavLink>
 
-    </div>
-)
+        </div>
+    )
 }
 export default NavBar
-
